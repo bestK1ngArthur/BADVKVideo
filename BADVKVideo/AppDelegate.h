ba803b6c86a75cd0241f8905510a3d24afc92455
Artem Belkov <bestK1ngArthur@aol.com>
2017-03-17 20:42:40 +0300

    Initial Commit

---
+++ BADVKVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BADVKVideo
+//
+//  Created by Artem Belkov on 17/03/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BADVKVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BADVKVideo
+//
+//  Created by Artem Belkov on 17/03/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
